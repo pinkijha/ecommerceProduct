1cd3f13258ac54b624dcc5e3b81e9bc388692503
Pinki Jha <pinkijha1310@gmail.com>
2025-02-10 11:39:22 +0530

    added product page with raw data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='flex shadow-md items-center justify-between'>
+      <div>
+        <img  className='md:h-[80px] rounded-full object-cover '
+        src='https://static.vecteezy.com/system/resources/previews/027/865/056/non_2x/shopping-logo-e-commerce-logotype-shooping-website-purple-gradient-women-s-shopping-website-fast-shopping-e-store-website-application-template-business-company-online-shop-vector.jpg' />
+      </div>
+      <div className='relative space-x-1 items-center'>
+        <input type='text' className='focus:ring-2 focus:ring-blue-100 outline-none border
+         border-slate-300 rounded-xl md:py-1 md:px-8'
+        placeholder='Search..' />
+        <span className='absolute z-10 left-2 text-lg top-2'><CiSearch /></span>
+      </div>
+      <div className='md:p-1 md:mr-4 text-3xl text-blue-700'>
+      <FaShoppingCart />
+      </div>
     </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FaCartPlus } from "react-icons/fa";
 
 const Products = () => {
   return (
-    <div>
-      
+    <div className='flex-flex-col space-y-2 md:w-[220px] rounded-lg shadow-md md:p-2 md:ml-[50px] md:mt-[20px] border border-gray-200'>
+      <img className='md:h-[200px] rounded-lg'
+      src='https://programmerowais.github.io/EcommerceProductPage.github.io/static/media/img1.520cc50bd13955f55cb2.jpg'/>
+      <p className='line-clamp-2  w-[200px]'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole,they'll withstand everything the weather can offer.</p>
+      <div className='flex justify-between items-center md:m-1'>
+      <p className='text-lg font-semibold'>$125.00</p>
+      <span className='text-2xl text-blue-700 cursor-pointer'><FaCartPlus /></span>
+      </div>
     </div>
   )
 }
